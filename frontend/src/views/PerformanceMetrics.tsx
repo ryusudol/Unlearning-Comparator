dee0957e86ec4d99354d2d5801c1f8352787e222
Suhyeon <fbtngus2789@naver.com>
2024-07-11 02:51:41 +0900

    feat: created PerformanceMetrics component

---
+++ frontend/src/views/PerformanceMetrics.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./PerformanceMetrics.module.css";
+
+export default function PerformanceMetrics() {
+  return <section className={styles.section}></section>;
+}
