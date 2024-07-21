238d0f8a75839cb67ca5d8b7f2be6fdff67337e1
Suhyeon <fbtngus2789@naver.com>
2024-07-22 01:00:41 +0900

    design: redesigning to 16:9 view

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,58 +3,17 @@ import styles from "./App.module.css";
 
 import Title from "../views/Title";
 import ContentBox from "../views/ContentBox";
-import SubTitle from "../views/SubTitle";
-import Button from "../views/Button";
-import SelectInput from "../views/SelectInput";
-import NumberInput from "../views/NumberInput";
-import SelectFromSubInput from "../views/SelectFromSubInput";
-import Embeddings from "../views/Embeddings";
-import PerformanceMetrics from "../views/PerformanceMetrics";
 
-const DATASETS = ["CIFAR-10"];
-const BATCH_SIZES = ["8", "16", "32", "64", "128", "256", "512"];
-const METHODS = ["method1", "method2", "method3", "method4"];
-const MODELS = ["ResNet18"];
-const SEEDS = ["1234"];
-const UNLEARN_CLASSES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+import Settings from "../sections/Settings";
+import Embeddings from "../sections/Embeddings";
+import PerformanceMetrics from "../sections/PerformanceMetrics";
 
 export default function App() {
   return (
     <section id={styles["body-wrapper"]}>
-      <div>
-        <Title title="Settings" />
-        <ContentBox height={45}>
-          <div className={styles["subset-wrapper"]}>
-            <SubTitle subtitle="Initial Settings" />
-            <SelectInput labelName="Model" optionData={MODELS} />
-            <SelectInput labelName="Dataset" optionData={DATASETS} />
-            <SelectFromSubInput
-              name="Method"
-              subNames={["Predefined", "Custom"]}
-              subTypes={["select", "file"]}
-              optionData={[METHODS]}
-            />
-            <SelectInput labelName="Seed" optionData={SEEDS} />
-            <SelectInput
-              labelName="Unlearn Class"
-              optionData={UNLEARN_CLASSES}
-              labelFontSize="md"
-            />
-          </div>
-          <div style={{ marginTop: "1rem" }} />
-          <div className={styles["subset-wrapper"]}>
-            <SubTitle subtitle="Training | Unlearning" />
-            <SelectInput labelName="Batch Size" optionData={BATCH_SIZES} />
-            <NumberInput labelName="Learning Rate" />
-            <NumberInput labelName="Epochs" />
-          </div>
-          <div style={{ marginTop: "0.6rem" }} />
-          <Button buttonText="Run" />
-        </ContentBox>
-        <ContentBox height={25}></ContentBox>
-        <Title title="Histories" />
-        <ContentBox height={20}></ContentBox>
-      </div>
+      <Settings />
+      <Title title="Histories" />
+      <ContentBox height={20}></ContentBox>
       <div>
         <Title title="Embeddings" />
         <Embeddings />
--- frontend/src/views/ContentBox.tsx
+++ frontend/src/views/ContentBox.tsx
@@ -3,21 +3,12 @@ import styles from "./ContentBox.module.css";
 
 type PropsType = {
   children?: React.ReactNode;
-  height: 20 | 25 | 45 | 50;
+  height: number;
 };
 
 export default function ContentBox({ children, height }: PropsType) {
-  const heightMode =
-    height === 20
-      ? "height-20"
-      : height === 25
-      ? "height-25"
-      : height === 45
-      ? "height-45"
-      : "height-50";
-
   return (
-    <section className={styles.section} id={styles[heightMode]}>
+    <section className={styles.section} style={{ height: `${height}px` }}>
       {children}
     </section>
   );
