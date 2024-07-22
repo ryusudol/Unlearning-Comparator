dce5a8c6bda8ec7aaa754433feaa208d3f8a686a
Suhyeon <fbtngus2789@naver.com>
2024-07-22 22:46:59 +0900

    feat: added cors processing

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import train, unlearn, inference, trained_models
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], # TODO: 배포 후 URL 수정
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(train.router)
 app.include_router(unlearn.router)
