19a488f7ae5a4324c9c549a87010ec76ec39f0fb
Suhyeon <fbtngus2789@naver.com>
2024-08-10 04:15:48 +0900

    refactor: integrated training and unlearning type files into one file

--- frontend/src/types/settings.ts
+++ frontend/src/types/settings.ts
@@ -2,7 +2,7 @@ export interface ClassAccuracies {
   [key: string]: number;
 }
 
-export interface Status {
+export interface TrainingStatus {
   is_training: boolean;
   current_epoch: number;
   total_epochs: number;
@@ -17,7 +17,20 @@ export interface Status {
   estimated_time_remaining: number;
 }
 
-export interface Configuration {
+export interface UnlearningStatus {
+  is_unlearning: boolean;
+  progress: number;
+  current_epoch: number;
+  total_epochs: number;
+  current_loss: number;
+  best_loss: number;
+  current_accuracy: number;
+  best_accuracy: number;
+  estimated_time_remaining: number;
+  forget_class: number;
+}
+
+export interface TrainingConfigurationData {
   model: string;
   dataset: string;
   epochs: number;
@@ -26,15 +39,30 @@ export interface Configuration {
   seed: number;
 }
 
+export interface UnlearningConfigurationData {
+  method: string;
+  forget_class: string;
+  epochs: number;
+  batch_size: number;
+  learning_rate: number;
+}
+
 export interface Action {
   type: string;
   payload: string | number;
 }
 
-export interface Props {
+export interface TrainingProps {
   operationStatus: number;
   setOperationStatus: (val: number) => void;
   setTrainedModels: (models: string[]) => void;
+}
+
+export interface UnlearningProps {
+  operationStatus: number;
+  setOperationStatus: (val: number) => void;
+  trainedModels: string[];
+  setUnlearnedModels: (models: string[]) => void;
 }
 
 export type Timer = ReturnType<typeof setInterval> | undefined;
