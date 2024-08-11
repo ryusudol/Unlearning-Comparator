b3746b19a2d06f2b8330bd2010393ff9c7bb55e4
Suhyeon <fbtngus2789@naver.com>
2024-08-11 22:55:02 +0900

    refactor: separated svg display part to an independent component

---
+++ frontend/src/components/UI/SvgViewer.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styles from "./SvgViewer.module.css";
+
+import SubTitle from "../SubTitle";
+
+interface Props {
+  mode: "r" | "u";
+  svgs: string[];
+  handleThumbnailClick: (e: React.MouseEvent<HTMLDivElement>) => void;
+  modifiedSvgs: string[];
+  selectedSvgId: number | undefined;
+}
+
+export default function SvgViewer({
+  mode,
+  svgs,
+  handleThumbnailClick,
+  modifiedSvgs,
+  selectedSvgId,
+}: Props) {
+  const createMarkup = (svg: string) => {
+    return { __html: svg };
+  };
+
+  const subtitle = mode === "r" ? "Retrained Model" : "Unlearned Model";
+
+  return (
+    <div className={styles.wrapper}>
+      <SubTitle subtitle={subtitle} />
+      {svgs && (
+        <div className={styles["content-wrapper"]}>
+          <div className={styles["svg-wrapper"]}>
+            <div
+              id={`${mode}-1`}
+              onClick={handleThumbnailClick}
+              className={styles.svg}
+              dangerouslySetInnerHTML={createMarkup(modifiedSvgs[0])}
+            />
+            <div
+              id={`${mode}-2`}
+              onClick={handleThumbnailClick}
+              className={styles.svg}
+              dangerouslySetInnerHTML={createMarkup(modifiedSvgs[1])}
+            />
+            <div
+              id={`${mode}-3`}
+              onClick={handleThumbnailClick}
+              className={styles.svg}
+              dangerouslySetInnerHTML={createMarkup(modifiedSvgs[2])}
+            />
+            <div
+              id={`${mode}-4`}
+              onClick={handleThumbnailClick}
+              className={styles.svg}
+              dangerouslySetInnerHTML={createMarkup(modifiedSvgs[3])}
+            />
+          </div>
+          {selectedSvgId && (
+            <div
+              className={styles["selected-svg"]}
+              dangerouslySetInnerHTML={createMarkup(svgs[selectedSvgId - 1])}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
