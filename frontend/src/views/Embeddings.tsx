e6822a2f9b0901c56f8117de8e0890e45c83a37e
Suhyeon <fbtngus2789@naver.com>
2024-08-14 04:33:10 +0900

    fix: resolved a problem with ProgressBar

--- frontend/src/views/Embeddings.tsx
+++ frontend/src/views/Embeddings.tsx
@@ -7,24 +7,12 @@ import SvgViewer from "../components/UI/SvgViewer";
 import { SvgContext } from "../store/svg-context";
 
 export default function Embeddings() {
-  const { retrieveRetrainedSvgs, retrieveUnlearnedSvgs } =
-    useContext(SvgContext);
+  const { retrainedSvgs, unlearnedSvgs } = useContext(SvgContext);
 
-  const retrainedSvgs = retrieveRetrainedSvgs();
-  const unlearnedSvgs = retrieveUnlearnedSvgs();
-
-  const [modifiedRetrainedSvgs, setModifiedRetrainedSvgs] = useState<string[]>(
-    []
-  );
-  const [modifiedUnlearnedSvgs, setModifiedUnlearnedSvgs] = useState<string[]>(
-    []
-  );
-  const [selectedRetrainedId, setSelectedRetrainedId] = useState<
-    number | undefined
-  >();
-  const [selectedUnlearnedId, setSelectedUnlearnedId] = useState<
-    number | undefined
-  >();
+  const [edittedRetrainSvgs, setEdittedetrainSvgs] = useState<string[]>([]);
+  const [EdittedUnlearnSvgs, setEdittedUnlearnSvgs] = useState<string[]>([]);
+  const [retrainFocus, setRetrainFocus] = useState<number | undefined>();
+  const [unlearnFocus, setUnlearnFocus] = useState<number | undefined>();
 
   useEffect(() => {
     const modifySvg = (svg: string) => {
@@ -35,19 +23,18 @@ export default function Embeddings() {
       legend.parentNode.removeChild(legend);
       return new XMLSerializer().serializeToString(svgDoc);
     };
-    const modifiedRetrainedSvgs = retrainedSvgs.map(modifySvg) as string[];
-    const modifiedUnlearnedSvgs = unlearnedSvgs.map(modifySvg) as string[];
-    setModifiedRetrainedSvgs(modifiedRetrainedSvgs);
-    setModifiedUnlearnedSvgs(modifiedUnlearnedSvgs);
-    setSelectedRetrainedId(retrainedSvgs ? 4 : undefined);
-    setSelectedUnlearnedId(unlearnedSvgs ? 4 : undefined);
+
+    setEdittedetrainSvgs(retrainedSvgs.map(modifySvg) as string[]);
+    setEdittedUnlearnSvgs(unlearnedSvgs.map(modifySvg) as string[]);
+    setRetrainFocus(retrainedSvgs ? 4 : undefined);
+    setUnlearnFocus(unlearnedSvgs ? 4 : undefined);
   }, [retrainedSvgs, unlearnedSvgs]);
 
   const handleThumbnailClick = (e: React.MouseEvent<HTMLDivElement>) => {
     const id = e.currentTarget.id;
     const idNum = +id.split("-")[1];
-    if (id.includes("r")) setSelectedRetrainedId(idNum);
-    else if (id.includes("u")) setSelectedUnlearnedId(idNum);
+    if (id.includes("r")) setRetrainFocus(idNum);
+    else if (id.includes("u")) setUnlearnFocus(idNum);
   };
 
   return (
@@ -59,16 +46,16 @@ export default function Embeddings() {
             mode="r"
             svgs={retrainedSvgs}
             handleThumbnailClick={handleThumbnailClick}
-            modifiedSvgs={modifiedRetrainedSvgs}
-            selectedSvgId={selectedRetrainedId}
+            modifiedSvgs={edittedRetrainSvgs}
+            selectedSvgId={retrainFocus}
           />
           <div className={styles.divider} />
           <SvgViewer
             mode="u"
             svgs={unlearnedSvgs}
             handleThumbnailClick={handleThumbnailClick}
-            modifiedSvgs={modifiedUnlearnedSvgs}
-            selectedSvgId={selectedUnlearnedId}
+            modifiedSvgs={EdittedUnlearnSvgs}
+            selectedSvgId={unlearnFocus}
           />
         </div>
       </ContentBox>
