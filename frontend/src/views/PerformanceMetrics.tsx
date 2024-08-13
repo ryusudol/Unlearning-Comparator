dd7e923108e408c140d8580ee62d0b2c7e37c35c
Suhyeon <fbtngus2789@naver.com>
2024-08-13 10:10:14 +0900

    feat: created CustomBarChart Component

--- frontend/src/views/PerformanceMetrics.tsx
+++ frontend/src/views/PerformanceMetrics.tsx
@@ -2,13 +2,23 @@ import styles from "./PerformanceMetrics.module.css";
 
 import Title from "../components/Title";
 import ContentBox from "../components/ContentBox";
+import CustomBarChart from "../components/CustomBarChart";
 
 export default function PerformanceMetrics() {
   return (
     <section className={styles["performance-metrics"]}>
       <Title title="Performance Metrics" />
       <ContentBox height={253}>
-        <div></div>
+        <div className={styles["barchart-row"]}>
+          <div>
+            <CustomBarChart dataKey="ua" color="4E79A7" />
+            <CustomBarChart dataKey="ra" color="F28E2B" />
+          </div>
+          <div>
+            <CustomBarChart dataKey="ta" color="E15759" />
+            <CustomBarChart dataKey="rte" color="76B7B2" />
+          </div>
+        </div>
       </ContentBox>
     </section>
   );
