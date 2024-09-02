7289cd7a2c6b2b1004189357aba28dce93d14a78
Suhyeon <fbtngus2789@naver.com>
2024-09-02 23:53:06 +0900

    feat: added Privacies feature

--- frontend/src/views/Privacies.tsx
+++ frontend/src/views/Privacies.tsx
@@ -1,6 +1,7 @@
 import styles from "./Privacies.module.css";
 
 import Title from "../components/Title";
+import SubTitle from "../components/SubTitle";
 import ContentBox from "../components/ContentBox";
 
 interface Props {
@@ -12,7 +13,32 @@ export default function Privacies({ height }: Props) {
     <section className={styles["privacy-attacks"]}>
       <Title title="Privacies" />
       <ContentBox height={height}>
-        <div className={styles.wrapper}></div>
+        <div className={styles.wrapper}>
+          <div className={styles["content-header"]}>
+            <div>
+              <SubTitle subtitle="Logit" fontSize={14} />
+              <img
+                className={styles["logit-img"]}
+                src="/logit.png"
+                alt="logit img"
+              />
+            </div>
+            <div>
+              <SubTitle subtitle="MIA" fontSize={14} />
+              <img
+                className={styles["mia-img"]}
+                src="/logit.png"
+                alt="mia img"
+              />
+            </div>
+          </div>
+          <SubTitle subtitle="Attack" fontSize={14} />
+          <img
+            className={styles["attack-img"]}
+            src="/attack.png"
+            alt="attack img"
+          />
+        </div>
       </ContentBox>
     </section>
   );
