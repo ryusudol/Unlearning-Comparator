5d36320cef56ae7a072153d0775a65a4ee7165f1
Suhyeon <fbtngus2789@naver.com>
2024-09-26 22:27:21 +0900

    design: modified the design of ConfigSelector component

--- frontend/src/components/ConfigSelector.tsx
+++ frontend/src/components/ConfigSelector.tsx
@@ -2,8 +2,11 @@ import { useContext } from "react";
 
 import { RunningStatusContext } from "../store/running-status-context";
 
+const commonStyle =
+  "w-full relative pl-3 pr-4 leading-5 text-[13px] font-[500] before:left-[-8px] before:w-3 before:h-3 before:absolute before:block before:border-l-[3px] before:border-l-[#fff] before:top-0 before:transform before:skew-x-[30deg] after:absolute after:block after:border-l-[3px] after:border-l-[#fff] after:transform after:skew-x-[-30deg] after:left-[-8px] after:w-3 after:h-3 after:top-3";
+
 interface Props {
-  mode: number;
+  mode: 0 | 1 | 2;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
@@ -11,39 +14,69 @@ export default function ConfigSelector({ mode, onClick }: Props) {
   const { isRunning } = useContext(RunningStatusContext);
 
   return (
-    <div className="w-3/5 h-[20px] bg-[#f0f0f0] flex justify-between items-center rounded-[4px] mt-[3px] mb-[6px] border-[0.5px] border-solid border-[#aeaeae]">
+    <div className="relative m-0 p-0 text-center w-3/4 overflow-hidden flex border-[1px] border-[rgba(0, 0, 0, 0.2)] top-[1px] rounded-t-[6px]">
       <button
-        className={`h-full text-[#6a7280] text-[13px] px-1 relative border-none cursor-pointer inline-block ${
-          mode === 0 &&
-          "bg-white text-[#030712] rounded-tl-[4px] rounded-bl-[4px]"
-        }`}
-        onClick={onClick}
         id="0"
-        disabled={isRunning}
+        onClick={onClick}
+        className={`${commonStyle} ${
+          mode === 0 ? "bg-black text-white" : "bg-white text-[#030712]"
+        }`}
       >
-        Training
+        <span>Training</span>
       </button>
       <button
-        className={`h-full text-[#6a7280] text-[13px] px-1 relative border-none cursor-pointer inline-block ${
-          mode === 1 && "bg-white text-[#030712]"
-        }`}
-        onClick={onClick}
         id="1"
-        disabled={isRunning}
+        onClick={onClick}
+        className={`${commonStyle} ${
+          mode === 1 ? "bg-black text-white" : "bg-white text-[#030712]"
+        } before:content-[''] before:bg-white after:content-[''] after:bg-white`}
       >
-        <span className="ml-[3px]">Unlearning</span>
+        <span>Unlearning</span>
       </button>
       <button
-        className={`h-full text-[#6a7280] text-[13px] px-1 relative border-none cursor-pointer inline-block ${
-          mode === 2 &&
-          "bg-white text-[#030712] rounded-tr-[4px] rounded-br-[4px]"
-        }`}
-        onClick={onClick}
         id="2"
-        disabled={isRunning}
+        onClick={onClick}
+        className={`${commonStyle} ${
+          mode === 2 ? "bg-black text-white" : "bg-white text-[#030712]"
+        } before:content-[''] before:bg-white after:content-[''] after:bg-white`}
       >
-        <span className="ml-[3px]">Defense</span>
+        <span>Defense</span>
       </button>
     </div>
+
+    // <div className="w-3/5 h-[20px] bg-[#f0f0f0] flex justify-between items-center rounded-[4px] mt-[3px] mb-[6px] border-[0.5px] border-solid border-[#aeaeae]">
+    //   <button
+    // className={`h-full text-[#6a7280] text-[13px] px-1 relative border-none cursor-pointer inline-block ${
+    //   mode === 0 &&
+    //   "bg-white text-[#030712] rounded-tl-[4px] rounded-bl-[4px]"
+    // }`}
+    // onClick={onClick}
+    // id="0"
+    // disabled={isRunning}
+    //   >
+    //     Training
+    //   </button>
+    //   <button
+    //     className={`h-full text-[#6a7280] text-[13px] px-1 relative border-none cursor-pointer inline-block ${
+    //       mode === 1 && "bg-white text-[#030712]"
+    //     }`}
+    //     onClick={onClick}
+    //     id="1"
+    //     disabled={isRunning}
+    //   >
+    //     <span className="ml-[3px]">Unlearning</span>
+    //   </button>
+    //   <button
+    //     className={`h-full text-[#6a7280] text-[13px] px-1 relative border-none cursor-pointer inline-block ${
+    //       mode === 2 &&
+    //       "bg-white text-[#030712] rounded-tr-[4px] rounded-br-[4px]"
+    //     }`}
+    //     onClick={onClick}
+    //     id="2"
+    //     disabled={isRunning}
+    //   >
+    //     <span className="ml-[3px]">Defense</span>
+    //   </button>
+    // </div>
   );
 }
