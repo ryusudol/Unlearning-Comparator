27224bc885d518a4a7b62d8a7e7b717237af8e03
Suhyeon <fbtngus2789@naver.com>
2024-10-02 00:02:33 +0900

    design: modified details of degisn in Predictions section

--- frontend/src/views/Predictions.tsx
+++ frontend/src/views/Predictions.tsx
@@ -26,10 +26,12 @@ export default function Predictions({ height }: Props) {
     >
       <div className="flex justify-between">
         <div className="flex items-center">
-          <div className="flex items-center">
+          {/* Title */}
+          <div className="flex items-center mr-2">
             <Target02Icon />
-            <h5 className="font-semibold ml-[3px] mr-5">Predictions</h5>
+            <h5 className="font-semibold ml-[3px]">Predictions</h5>
           </div>
+          {/* Icon Buttons */}
           <div className="flex items-center">
             <ChartBubble02Icon className="cursor-pointer scale-90" />
             <div className="relative cursor-pointer ml-[1px]">
@@ -47,6 +49,7 @@ export default function Predictions({ height }: Props) {
             <ZoomInAreaIcon className="cursor-pointer scale-90" />
           </div>
         </div>
+        {/* Dataset Selector */}
         <div className="flex items-center">
           <span className="text-xs font-light mr-2">Dataset:</span>
           <RadioGroup className="flex" defaultValue="training">
@@ -69,6 +72,7 @@ export default function Predictions({ height }: Props) {
         <span className="text-[11px] font-extralight -rotate-90 -mr-6 -ml-3">
           Ground Truth
         </span>
+        {/* Bubble Chart 1 */}
         <div
           id="baseline-bubble-chart"
           className="flex flex-col items-center -mt-1.5 mr-1"
@@ -77,9 +81,12 @@ export default function Predictions({ height }: Props) {
             <NeuralNetworkIcon className="mr-[3px]" />
             <span className="text-[15px]">Baseline Model (id01)</span>
           </div>
-          <img src="/bubble.png" alt="bubble chart img" />
-          <span className="text-[11px] font-extralight -mt-1">Prediction</span>
+          <img src="/bubble.png" alt="bubble chart img" className="h-[200px]" />
+          <span className="text-[11px] font-extralight -mt-[5px]">
+            Prediction
+          </span>
         </div>
+        {/* Bubble Chart 2 */}
         <div
           id="comparison-bubble-chart"
           className="flex flex-col items-center -mt-1.5"
@@ -88,9 +95,12 @@ export default function Predictions({ height }: Props) {
             <GitCompareIcon className="mr-[3px]" />
             <span className="text-[15px]">Comparison Model (id02)</span>
           </div>
-          <img src="/bubble.png" alt="bubble chart img" />
-          <span className="text-[11px] font-extralight -mt-1">Prediction</span>
+          <img src="/bubble.png" alt="bubble chart img" className="h-[200px]" />
+          <span className="text-[11px] font-extralight -mt-[5px]">
+            Prediction
+          </span>
         </div>
+        {/* Legend */}
         <div className="flex flex-col items-center">
           <BubbleLegend scale={sizeScale} />
           <img src="/bubble-legend.png" alt="bubble legend img" />
