d4367ffda21ae6357e760079279239d4dde8e3a2
Suhyeon <fbtngus2789@naver.com>
2024-10-27 18:40:05 +0900

    design: changed the checkbox from the DataTable to radio

--- frontend/src/components/UI/radio-group.tsx
+++ frontend/src/components/UI/radio-group.tsx
@@ -26,7 +26,7 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-[10px] w-[10px] rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "aspect-square h-2.5 w-2.5 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
