3ab0b6ecd77621b0b9d31905323e589ebf810633
gnueaj <dlwodnd00@gmail.com>
2024-11-10 23:17:08 +0900

    modify default cutmix ratio

--- backend/app/utils/helpers.py
+++ backend/app/utils/helpers.py
@@ -59,7 +59,7 @@ def get_data_loaders(batch_size):
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 
-        Cutout(n_holes=1, length=16, p=0.5) #
+        Cutout(n_holes=1, length=16, p=1.0) #
     ])
     
     test_transform = transforms.Compose([
