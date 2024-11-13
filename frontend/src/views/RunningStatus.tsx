731d4a9ed420b2c06c46f9139242e4f2ed550c29
Suhyeon <fbtngus2789@naver.com>
2024-11-13 15:15:13 +0900

    refactor: created a new function for generating steps data and removed unnecessary codes

--- frontend/src/views/RunningStatus.tsx
+++ frontend/src/views/RunningStatus.tsx
@@ -2,13 +2,14 @@ import { useState, useEffect, useContext, useCallback } from "react";
 import { Check, Dot, Loader2 } from "lucide-react";
 
 import { Button } from "../components/UI/button";
-import { fetchDataFile } from "../https/unlearning";
+import { fetchDataFile } from "../utils/api/unlearning";
 import { ExperimentsContext } from "../store/experiments-context";
 import { RunningStatusContext } from "../store/running-status-context";
 import { BaselineComparisonContext } from "../store/baseline-comparison-context";
-import { fetchUnlearningStatus, cancelUnlearning } from "../https/utils";
+import { fetchUnlearningStatus, cancelUnlearning } from "../utils/api/requests";
 import { VitalIcon } from "../components/UI/icons";
 import { ForgetClassContext } from "../store/forget-class-context";
+import { getProgressSteps } from "../utils/getProgressSteps";
 import {
   Stepper,
   StepperDescription,
@@ -27,7 +28,6 @@ export default function RunningStatus({ height }: { height: number }) {
     status,
     activeStep,
     updateIsRunning,
-    initStatus,
     updateStatus,
     updateActiveStep,
   } = useContext(RunningStatusContext);
@@ -37,61 +37,14 @@ export default function RunningStatus({ height }: { height: number }) {
 
   const isFirstRunning = status.total_epochs === 0;
   const progress = status.progress;
-
-  const steps = [
-    {
-      step: 1,
-      title: "Unlearn",
-      description: `Method: ${status.method ? status.method : "-"} | Epochs: ${
-        isFirstRunning ? "-" : status.current_epoch + "/" + status.total_epochs
-      }\nUnlearning Loss: ${
-        status.current_unlearn_loss === 0
-          ? "-"
-          : status.current_unlearn_loss.toFixed(3)
-      } | Unlearning Accuracy: ${
-        status.current_unlearn_accuracy === 0
-          ? "-"
-          : status.current_unlearn_accuracy
-      }`,
-    },
-    {
-      step: 2,
-      title: "Evaluate",
-      description: `Training Loss: ${
-        status.p_training_loss === 0 ? "-" : status.p_training_loss
-      } | Training Accuracy: ${
-        status.p_training_accuracy === 0 ? "-" : status.p_training_accuracy
-      }\nTest Loss: ${
-        status.p_test_loss === 0 ? "-" : status.p_test_loss
-      } | Test Accuracy: ${
-        status.p_test_accuracy === 0 ? "-" : status.p_test_accuracy
-      }`,
-    },
-    {
-      step: 3,
-      title: "Analyze",
-      description: `${
-        (activeStep === 3 &&
-          (progress.includes("UMAP") || progress.includes("CKA"))) ||
-        (!isFirstRunning && progress === "Idle")
-          ? `Computing UMAP Embedding... ${
-              !progress.includes("UMAP") ? "100" : umapProgress
-            }%`
-          : "Computing UMAP Embedding"
-      }\n${
-        (activeStep === 3 && progress.includes("CKA")) ||
-        (!isFirstRunning && progress === "Idle")
-          ? `Calculating CKA Similarity... ${
-              progress === "Idle" ? "100" : ckaProgress
-            }%`
-          : "Calculating CKA Similarity"
-      }\n${
-        !isFirstRunning && progress === "Idle"
-          ? `Done! Experiment ID: ${status.recent_id}`
-          : ""
-      }`,
-    },
-  ];
+  const steps = getProgressSteps(
+    status,
+    isFirstRunning,
+    activeStep,
+    progress,
+    umapProgress,
+    ckaProgress
+  );
 
   const checkStatus = useCallback(async () => {
     try {
@@ -136,10 +89,6 @@ export default function RunningStatus({ height }: { height: number }) {
   ]);
 
   useEffect(() => {
-    initStatus();
-  }, [initStatus]);
-
-  useEffect(() => {
     let intervalId: NodeJS.Timeout | null = null;
 
     if (isRunning) {
@@ -155,57 +104,30 @@ export default function RunningStatus({ height }: { height: number }) {
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout | null = null;
+    const startTime = Date.now();
+    const duration = 10000;
 
-    if (progress.includes("UMAP")) {
-      const startTime = Date.now();
-      const duration = 10000;
+    intervalId = setInterval(() => {
+      const elapsed = Date.now() - startTime;
+      const progressValue = Math.min(
+        Math.floor((elapsed / duration) * 100),
+        100
+      );
 
-      intervalId = setInterval(() => {
-        const elapsed = Date.now() - startTime;
-        const progressValue = Math.min(
-          Math.floor((elapsed / duration) * 100),
-          100
-        );
+      if (status.progress.includes("UMAP")) setUmapProgress(progressValue);
+      else setCkaProgress(progressValue);
 
-        setUmapProgress(progressValue);
-
-        if (progressValue === 100) {
-          clearInterval(intervalId!);
-        }
-      }, 100);
-    } else if (progress.includes("CKA")) {
-      const startTime = Date.now();
-      const duration = 10000;
-
-      intervalId = setInterval(() => {
-        const elapsed = Date.now() - startTime;
-        const progressValue = Math.min(
-          Math.floor((elapsed / duration) * 100),
-          100
-        );
-
-        setCkaProgress(progressValue);
-
-        if (progressValue === 100) {
-          clearInterval(intervalId!);
-        }
-      }, 100);
-    }
+      if (progressValue === 100) {
+        clearInterval(intervalId!);
+      }
+    }, 100);
 
     return () => {
       if (intervalId) {
         clearInterval(intervalId);
       }
     };
-  }, [progress]);
-
-  // const handleCancelClick = async () => {
-  //   if (window.confirm("Are you sure you want to cancel the experiment?")) {
-  //     await cancelUnlearning();
-  //     updateIsRunning(false);
-  //     initStatus();
-  //   }
-  // };
+  }, [status.progress]);
 
   return (
     <section
@@ -217,7 +139,7 @@ export default function RunningStatus({ height }: { height: number }) {
         <h5 className="font-semibold ml-1 text-lg">Progress</h5>
       </div>
       <div>
-        <Stepper className="mx-auto flex w-full max-w-md flex-col justify-start gap-1.5">
+        <Stepper className="mx-auto flex w-full flex-col justify-start gap-1.5">
           {steps.map((step, idx) => {
             const isNotLastStep = idx !== steps.length - 1;
 
@@ -250,10 +172,10 @@ export default function RunningStatus({ height }: { height: number }) {
                   </Button>
                 </StepperTrigger>
                 <div className="flex flex-col">
-                  <StepperTitle className="text-sm font-semibold transition lg:text-base">
+                  <StepperTitle className="font-semibold transition text-base">
                     {step.title}
                   </StepperTitle>
-                  <StepperDescription className="text-xs text-muted-foreground whitespace-pre-line transition md:not-sr-only lg:text-sm">
+                  <StepperDescription className="text-muted-foreground whitespace-pre-line transition text-sm">
                     {step.description.split("\n").map((el, idx) => (
                       <p key={idx}>{el}</p>
                     ))}
@@ -264,21 +186,6 @@ export default function RunningStatus({ height }: { height: number }) {
           })}
         </Stepper>
       </div>
-      {/* {isRunning && (
-        <CustomButton
-          onClick={handleCancelClick}
-          content={
-            <>
-              <MultiplicationSignIcon
-                className="w-5 h-5 mr-0.5"
-                color="white"
-              />
-              <span className="text-base">Cancel</span>
-            </>
-          }
-          className="flex items-center w-16 bg-red-600 hover:bg-red-700"
-        />
-      )} */}
     </section>
   );
 }
