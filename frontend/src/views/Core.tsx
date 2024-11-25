0f2092e5346bd3ce2e21269cf4691ffaefca0cec
Suhyeon <fbtngus2789@naver.com>
2024-11-25 18:23:05 +0900

    feat: added a legend for scatter plots

--- frontend/src/views/Core.tsx
+++ frontend/src/views/Core.tsx
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 
 import Embeddings from "./Embeddings";
 import PrivacyAttack from "./PrivacyAttack";
-import { ChartScatterIcon } from "../components/UI/icons";
+import { Separator } from "../components/UI/separator";
+import { forgetClassNames } from "../constants/forgetClassNames";
+import { TABLEAU10 } from "../constants/tableau10";
+import {
+  ChartScatterIcon,
+  CircleIcon,
+  MultiplicationSignIcon,
+} from "../components/UI/icons";
 
 const EMBEDDINGS = "embeddings";
 const ATTACK = "attack";
@@ -24,37 +31,72 @@ export default function Core({ height }: { height: number }) {
       style={{ height }}
       className="w-[1312px] p-1 border-[1px] border-solid border-[rgba(0, 0, 0, 0.2)]"
     >
-      <div className="flex items-center mb-0.5">
-        <div
-          id={EMBEDDINGS}
-          onClick={handleDisplayModeChange}
-          className={`relative z-10 flex items-center mr-3 cursor-pointer pb-0.5 px-1 ${
-            !isEmbeddingMode && "text-gray-400 border-none"
-          }`}
-        >
-          <ChartScatterIcon className={!isEmbeddingMode ? "opacity-40" : ""} />
-          <button className="font-semibold ml-[3px] text-lg">Embeddings</button>
-          {isEmbeddingMode && (
-            <div className="absolute w-full h-0.5 bg-black right-0 bottom-[3px]" />
-          )}
+      <div className="flex justify-between items-center">
+        <div className="flex items-center mb-0.5">
+          <div
+            id={EMBEDDINGS}
+            onClick={handleDisplayModeChange}
+            className={`relative z-10 flex items-center mr-3 cursor-pointer pb-0.5 px-1 ${
+              !isEmbeddingMode && "text-gray-400 border-none"
+            }`}
+          >
+            <ChartScatterIcon
+              className={!isEmbeddingMode ? "opacity-40" : ""}
+            />
+            <button className="font-semibold ml-[3px] text-lg">
+              Embeddings
+            </button>
+            {isEmbeddingMode && <UnderLine />}
+          </div>
+          <div
+            onClick={handleDisplayModeChange}
+            className={`relative z-10 flex items-center cursor-pointer pb-0.5 px-1 ${
+              isEmbeddingMode && "text-gray-400 border-none"
+            }`}
+          >
+            <img
+              src="/hacker.png"
+              alt="Attack logo img"
+              className={`w-4 ${isEmbeddingMode && "opacity-40"}`}
+            />
+            <button id={ATTACK} className="font-semibold ml-[3px] text-lg">
+              Privacy Attack
+            </button>
+            {!isEmbeddingMode && <UnderLine />}
+          </div>
         </div>
-        <div
-          onClick={handleDisplayModeChange}
-          className={`relative z-10 flex items-center cursor-pointer pb-0.5 px-1 ${
-            isEmbeddingMode && "text-gray-400 border-none"
-          }`}
-        >
-          <img
-            src="/hacker.png"
-            alt="Attack logo img"
-            className={`w-4 ${isEmbeddingMode && "opacity-40"}`}
+        <div className="flex items-center border border-b-white rounded-t-[6px] px-2 py-1 relative top-[1px]">
+          <div className="flex items-center mr-5">
+            <span className="text-[15px] font-medium mr-2.5">Data Type</span>
+            <ul className="flex items-center gap-2.5">
+              <li className="flex items-center">
+                <CircleIcon className="w-3 h-3 mr-1.5 text-[#4f5562]" />
+                <span className="text-[13px]">Remaining Data</span>
+              </li>
+              <li className="flex items-center">
+                <MultiplicationSignIcon className="text-[#4f5562] mr-1.5" />
+                <span className="text-[13px]">Unlearning Target</span>
+              </li>
+            </ul>
+          </div>
+          <div className="flex items-center">
+            <span className="text-[15px] font-medium mr-2.5">Prediction</span>
+            <ul className="flex items-center gap-2.5">
+              {forgetClassNames.map((name, idx) => (
+                <li key={idx} className="flex items-center">
+                  <div
+                    style={{ backgroundColor: TABLEAU10[idx] }}
+                    className="w-3.5 h-3.5 mr-1"
+                  />
+                  <span className="text-[13px]">{name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <Separator
+            orientation="horizontal"
+            className="absolute bottom-0 h-[1px] w-[calc(100%-16px)]"
           />
-          <button id={ATTACK} className="font-semibold ml-[3px] text-lg">
-            Privacy Attack
-          </button>
-          {!isEmbeddingMode && (
-            <div className="absolute w-full h-0.5 bg-black right-0 bottom-[3px]" />
-          )}
         </div>
       </div>
       {isEmbeddingMode ? (
@@ -63,5 +105,11 @@ export default function Core({ height }: { height: number }) {
         <PrivacyAttack height={HEIGHT} />
       )}
     </section>
+  );
+}
+
+function UnderLine() {
+  return (
+    <div className="absolute w-full h-0.5 bg-black right-0 bottom-[3px]" />
   );
 }
