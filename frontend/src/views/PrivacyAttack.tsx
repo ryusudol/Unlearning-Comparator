985c7247ba1613364602dbf62fd42fc625474ad8
Suhyeon <fbtngus2789@naver.com>
2024-12-22 20:32:59 +0900

    feat: implemented an interactive distribution dot plot

--- frontend/src/views/PrivacyAttack.tsx
+++ frontend/src/views/PrivacyAttack.tsx
@@ -1,8 +1,12 @@
+import DistributionDotPlot from "../components/DistributionDotPlot";
+
 export default function PrivacyAttack({ height }: { height: number }) {
   return (
     <div
-      style={{ height: `${height}px` }}
+      style={{ height }}
       className="w-full flex justify-evenly items-center border-[1px] border-solid border-[rgba(0, 0, 0, 0.2)] rounded-[6px]"
-    ></div>
+    >
+      <DistributionDotPlot />
+    </div>
   );
 }
