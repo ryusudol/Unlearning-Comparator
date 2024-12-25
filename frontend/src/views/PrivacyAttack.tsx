ee8c9535ec2a9193833c361c729846a3a81413f4
Suhyeon <fbtngus2789@naver.com>
2024-12-25 09:09:12 +0900

    feat: temporarily removed distribution dot plots from the privacy attack view

--- frontend/src/views/PrivacyAttack.tsx
+++ frontend/src/views/PrivacyAttack.tsx
@@ -1,13 +1,8 @@
-import Discriminator from "../components/Discriminator";
-
 export default function PrivacyAttack({ height }: { height: number }) {
   return (
     <div
       style={{ height }}
       className="w-full flex justify-evenly items-center border-[1px] border-solid border-[rgba(0, 0, 0, 0.2)] rounded-[6px]"
-    >
-      <Discriminator mode="Baseline" />
-      <Discriminator mode="Comparison" />
-    </div>
+    ></div>
   );
 }
