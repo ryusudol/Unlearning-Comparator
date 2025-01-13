540a6441ac4a9c0b9a7e531d42dc2f53e6d2dcbb
Suhyeon <fbtngus2789@naver.com>
2025-01-13 15:22:16 +0900

    feat: introduced the useModelSelection custom hook to the Accuracies view code

--- frontend/src/views/Accuracies.tsx
+++ frontend/src/views/Accuracies.tsx
@@ -5,18 +5,19 @@ import Title from "../components/Title";
 import Indicator from "../components/Indicator";
 import VerticalBarChart from "../components/Accuracies/VerticalBarChart";
 import { useForgetClass } from "../hooks/useForgetClass";
+import { useModelSelection } from "../hooks/useModelSelection";
 import { ViewProps } from "../types/common";
 import { Chart01Icon } from "../components/UI/icons";
-import { BaselineComparisonContext } from "../store/baseline-comparison-context";
 import { ExperimentsContext } from "../store/experiments-context";
 import { getAccuracyGap, getMaxGap } from "../utils/data/accuracies";
 
 export default function Accuracies({ width, height }: ViewProps) {
-  const { baseline, comparison } = useContext(BaselineComparisonContext);
   const { baselineExperiment, comparisonExperiment } =
     useContext(ExperimentsContext);
 
+  const { areAllModelsSelected } = useModelSelection();
   const { forgetClassExist } = useForgetClass();
+  console.log("re-rendered!");
 
   const [hoveredClass, setHoveredClass] = useState<string | null>(null);
 
@@ -35,8 +36,6 @@ export default function Accuracies({ width, height }: ViewProps) {
 
   const maxGap = Math.max(trainMaxGap, testMaxGap);
 
-  const bothBaseCompExist = baseline !== "" && comparison !== "";
-
   return (
     <View width={width} height={height} className="border-t-0">
       <Title
@@ -48,7 +47,7 @@ export default function Accuracies({ width, height }: ViewProps) {
         }
       />
       {forgetClassExist ? (
-        bothBaseCompExist ? (
+        areAllModelsSelected ? (
           <div className="w-full flex items-center relative bottom-0.5">
             <VerticalBarChart
               mode="Training"
