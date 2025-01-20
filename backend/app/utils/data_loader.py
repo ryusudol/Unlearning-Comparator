c4541265cbc39250f7de6b58c13c9b9807449c6c
gnueaj <dlwodnd00@gmail.com>
2025-01-20 16:34:10 +0900

    fixed download logic

--- backend/app/utils/data_loader.py
+++ backend/app/utils/data_loader.py
@@ -4,26 +4,16 @@ import numpy as np
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-def load_cifar10_batch(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict[b'data'], dict[b'labels']
-
 def load_cifar10_data():
-    data_dir = 'data/cifar-10-batches-py'
-    x_train = []
-    y_train = []
-
-    for i in range(1, 6):
-        filename = os.path.join(data_dir, f'data_batch_{i}')
-        X, Y = load_cifar10_batch(filename)
-        x_train.append(X)
-        y_train.append(Y)
-
-    x_train = np.concatenate(x_train)
-    y_train = np.concatenate(y_train)
-
-    return x_train.reshape(50000, 3, 32, 32).transpose(0, 2, 3, 1), y_train
+    """Load CIFAR-10 training data with automatic download"""
+    # Use torchvision's CIFAR10 dataset to handle downloading
+    train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=None)
+    
+    # Convert to numpy array in the format we need
+    x_train = train_set.data  # This is already in (N, 32, 32, 3) format
+    y_train = np.array(train_set.targets)
+    
+    return x_train, y_train
 
 def get_data_loaders(batch_size, augmentation=False):
     base_transforms = [
