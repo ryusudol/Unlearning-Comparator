4fb50156fe06cc6ff9a2a602a2752f8dba0f5cf8
Suhyeon <fbtngus2789@naver.com>
2025-02-01 22:07:25 +0900

    feat: enabled the selection of the Dataset and the Model after getting into the system

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,9 +4,17 @@ import ForgetClassTabs from "./ForgetClassTabs";
 import { LogoIcon, GithubIcon } from "../UI/icons";
 import { DatasetContext } from "../../store/dataset-context";
 import { NeuralNetworkModelContext } from "../../store/neural-network-model-context";
+import { DATASETS, NEURAL_NETWORK_MODELS } from "../../constants/common";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../UI/select";
 
 export default function Header() {
-  const { dataset } = useContext(DatasetContext);
+  const { dataset, saveDataset } = useContext(DatasetContext);
   const { neuralNetworkModel } = useContext(NeuralNetworkModelContext);
 
   const handleGithubIconClick = () => {
@@ -14,6 +22,10 @@ export default function Header() {
       "https://github.com/gnueaj/Machine-Unlearning-Comparator",
       "_blank"
     );
+  };
+
+  const handleDatasetChange = (dataset: string) => {
+    saveDataset(dataset);
   };
 
   return (
@@ -36,13 +48,41 @@ export default function Header() {
       <div className="flex absolute left-[1312px] top-4 text-[13px]">
         <div className="flex flex-col mr-10">
           <span className="text-[10px] text-gray-300">Dataset</span>
-          <span className="text-xs font-semibold -mt-[3px]">{dataset}</span>
+          <Select
+            defaultValue={dataset}
+            onValueChange={handleDatasetChange}
+            name="dataset"
+          >
+            <SelectTrigger className="h-[10px] text-xs font-semibold p-0 bg-transparent focus:outline-none focus:ring-0 border-none">
+              <SelectValue placeholder={dataset} />
+            </SelectTrigger>
+            <SelectContent>
+              {DATASETS.map((dataset, idx) => (
+                <SelectItem key={idx} value={dataset}>
+                  {dataset}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
         <div className="flex flex-col">
           <span className="text-[10px] text-gray-300">Model</span>
-          <span className="text-xs font-semibold -mt-[3px]">
-            {neuralNetworkModel}
-          </span>
+          <Select
+            defaultValue={neuralNetworkModel}
+            onValueChange={handleDatasetChange}
+            name="neuralNetworkModel"
+          >
+            <SelectTrigger className="h-[10px] text-xs font-semibold p-0 bg-transparent focus:outline-none focus:ring-0 border-none">
+              <SelectValue placeholder={neuralNetworkModel} />
+            </SelectTrigger>
+            <SelectContent>
+              {NEURAL_NETWORK_MODELS.map((neuralNetworkModel, idx) => (
+                <SelectItem key={idx} value={neuralNetworkModel}>
+                  {neuralNetworkModel}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
       </div>
     </div>
