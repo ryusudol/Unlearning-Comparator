0910c7e464da7f8acebd759a712afa5024d81794
Suhyeon <fbtngus2789@naver.com>
2025-02-10 00:44:30 +0900

    design: changed the design of the attack configuration component

--- frontend/src/views/PrivacyAttack.tsx
+++ frontend/src/views/PrivacyAttack.tsx
@@ -4,19 +4,24 @@ import View from "../components/View";
 import AttackConfiguration from "../components/PrivacyAttack/AttackConfiguration";
 import AttackAnalytics from "../components/PrivacyAttack/AttackAnalytics";
 import { Separator } from "../components/UI/separator";
-import { ATTACK_METHODS, THRESHOLD_METHODS } from "../constants/privacyAttack";
+import {
+  ATTACK_METHODS,
+  THRESHOLD_STRATEGIES,
+} from "../constants/privacyAttack";
 
 export default function PrivacyAttack({ height }: { height: number }) {
   const [attackMethod, setAttackMethod] = useState(ATTACK_METHODS[0]);
-  const [thresholdMethod, setThresholdMethod] = useState(THRESHOLD_METHODS[0]);
+  const [thresholdStrategy, setThresholdStrategy] = useState(
+    THRESHOLD_STRATEGIES[0].strategy
+  );
 
   const handleAttackMethodChange = (method: string) => {
     setAttackMethod(method);
   };
 
   const handleThresholdMethodClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    const method = e.currentTarget.innerHTML;
-    setThresholdMethod(method);
+    const method = e.currentTarget.id;
+    setThresholdStrategy(method);
   };
 
   return (
@@ -26,7 +31,7 @@ export default function PrivacyAttack({ height }: { height: number }) {
     >
       <AttackConfiguration
         onAttackChange={handleAttackMethodChange}
-        thresholdMethod={thresholdMethod}
+        thresholdStrategy={thresholdStrategy}
         onThresholdChange={handleThresholdMethodClick}
       />
       <Separator orientation="vertical" className="h-[612px] w-[1px] mx-1.5" />
