48dd267178303b4c960b018e658e269f684a7189
Suhyeon <fbtngus2789@naver.com>
2025-02-11 15:20:53 +0900

    feat: removed AttackConfiguration and widened the width of attack plots

--- frontend/src/views/PrivacyAttack.tsx
+++ frontend/src/views/PrivacyAttack.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import View from "../components/View";
-import AttackConfiguration from "../components/PrivacyAttack/AttackConfiguration";
+// import AttackConfiguration from "../components/PrivacyAttack/AttackConfiguration";
 import AttackAnalytics from "../components/PrivacyAttack/AttackAnalytics";
 import { Separator } from "../components/UI/separator";
 import {
@@ -31,7 +31,7 @@ export default function PrivacyAttack({ height }: { height: number }) {
       height={height}
       className="w-full flex items-center rounded-[6px] px-1.5"
     >
-      <AttackConfiguration
+      {/* <AttackConfiguration
         onAttackChange={handleAttackMethodChange}
         thresholdStrategy={thresholdStrategy}
         onThresholdChange={handleThresholdMethodClick}
@@ -39,7 +39,7 @@ export default function PrivacyAttack({ height }: { height: number }) {
       <Separator
         orientation="vertical"
         className="h-[612px] w-[1px] ml-[9px] mr-3.5"
-      />
+      /> */}
       <AttackAnalytics
         mode="Baseline"
         thresholdStrategy={thresholdStrategy}
