a4aeadb7a94620a3ae6213fd2de2e3d23b88c955
Suhyeon <fbtngus2789@naver.com>
2025-02-14 23:47:02 +0900

    feat: added a document icon to Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { FileText } from "lucide-react";
 
 import ForgetClassTabs from "./ForgetClassTabs";
 import { LogoIcon, GithubIcon } from "../UI/icons";
@@ -84,10 +85,13 @@ export default function Header() {
           <ForgetClassTabs />
         </div>
       </div>
-      <GithubIcon
-        onClick={handleGithubIconClick}
-        className="w-7 h-7 cursor-pointer"
-      />
+      <div className="flex items-center gap-3">
+        <FileText className="w-7 h-7 cursor-pointer" />
+        <GithubIcon
+          onClick={handleGithubIconClick}
+          className="w-7 h-7 cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
