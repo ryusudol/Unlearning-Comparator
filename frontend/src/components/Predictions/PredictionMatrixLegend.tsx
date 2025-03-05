df8246f805ca3b5fe0a57bb6416101c3da92156d
Suhyeon <fbtngus2789@naver.com>
2025-03-06 02:42:32 +0900

    feat: implemented the new design of prediction matrix legend

--- frontend/src/components/Predictions/PredictionMatrixLegend.tsx
+++ frontend/src/components/Predictions/PredictionMatrixLegend.tsx
@@ -14,46 +14,105 @@ export default function PredictionMatrixLegend() {
 
   return (
     <div className="flex justify-center items-center gap-11 text-[#666666] mb-1">
-      <div className="grid grid-cols-[1fr,auto,1fr] gap-y-1.5 place-items-center text-[11px] grid-rows-[18px_14px] relative left-[35px]">
-        <div className="w-1 h-1 rounded-full bg-[#666666]" />
-        <div className="w-[13px] h-[13px] rounded-full bg-[#666666]" />
-        <div className="w-[18px] h-[18px] rounded-full bg-[#666666]" />
-        <p className="flex flex-col items-center relative top-1">
-          <span className="text-base relative top-1">Small</span>
-          <span className="text-xs">Proportion</span>
-        </p>
-        <Arrow className="mx-4" />
-        <p className="flex flex-col items-center relative top-1">
-          <span className="text-base relative top-1">Large</span>
-          <span className="text-xs">Proportion</span>
-        </p>
+      <div className="flex items-center relative left-[34px]">
+        <RightArrowIcon />
+        <div className="flex flex-col leading-4">
+          <span className="text-xs">Row</span>
+          <span>Proportion</span>
+        </div>
+        <RectangleIcon className="mx-1.5" />
+        <div className="flex flex-col items-end leading-4">
+          <span className="text-xs">Row</span>
+          <span>Confidence</span>
+        </div>
+        <LeftArrowIcon />
       </div>
 
       <div className="flex flex-col items-center gap-y-1.5 relative top-0.5 left-4">
         <div className="relative top-2 w-[156px] h-3.5">
-          <div
-            className="w-full h-full relative top-1"
-            style={{ background: gradient }}
-          />
-          <div className="absolute -bottom-[7.5px] left-1">
+          <div className="w-full h-full" style={{ background: gradient }} />
+          <div className="absolute -bottom-[3.5px] left-1">
             <span className="text-[10px] text-black">0</span>
           </div>
-          <div className="absolute -bottom-[7.5px] right-1">
+          <div className="absolute -bottom-[3.5px] right-1">
             <span className="text-[10px] text-white">1</span>
           </div>
         </div>
-        <div className="text-nowrap flex items-center gap-0 text-[11px]">
-          <p className="flex flex-col items-start relative top-1 left-2">
-            <span className="text-base relative top-1">Low</span>
-            <span className="text-xs">Confidence</span>
-          </p>
-          <Arrow className="mx-2" />
-          <p className="flex flex-col items-end relative top-1 right-2">
-            <span className="text-base relative top-1">High</span>
-            <span className="text-xs">Confidence</span>
-          </p>
+        <div className="flex items-center text-[11px]">
+          <span className="text-base relative top-1 left-2">Low</span>
+          <Arrow className="mx-[33px] relative top-1" />
+          <span className="text-base relative top-1 right-2">High</span>
         </div>
       </div>
     </div>
+  );
+}
+
+function RectangleIcon({ className }: { className: string }) {
+  return (
+    <svg
+      className={className}
+      width="35"
+      height="35"
+      viewBox="0 0 35 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="0.25"
+        y="0.25"
+        width="34.5"
+        height="34.5"
+        fill="#808080"
+        stroke="white"
+        stroke-width="0.5"
+      />
+      <line
+        x1="0.285663"
+        y1="0.588543"
+        x2="33.9734"
+        y2="34.2763"
+        stroke="white"
+        stroke-width="0.5"
+      />
+    </svg>
+  );
+}
+
+function RightArrowIcon() {
+  return (
+    <svg
+      className="absolute left-8 top-7"
+      width="51"
+      height="18"
+      viewBox="0 0 51 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.638278 7.7551L12.0152 5.78077L8.03658 16.6207L0.638278 7.7551ZM50.2693 2.13884C40.5943 13.7904 32.3344 17.7611 25.2677 17.9246C18.2512 18.0869 12.6936 14.4714 8.52946 11.6868L9.64123 10.0242C13.8373 12.8303 18.9095 16.0711 25.2215 15.9251C31.4833 15.7802 39.2506 12.2778 48.7307 0.861163L50.2693 2.13884Z"
+        fill="black"
+        fill-opacity="0.5"
+      />
+    </svg>
+  );
+}
+
+function LeftArrowIcon() {
+  return (
+    <svg
+      className="absolute right-8 bottom-5"
+      width="47"
+      height="15"
+      viewBox="0 0 47 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M46.0979 14.0036L34.627 12.6799L41.5088 3.40765L46.0979 14.0036ZM0.343022 13.2496C11.9721 3.11568 20.2958 -0.124575 26.5694 0.234259C32.8909 0.59583 36.8133 4.594 39.6411 8.0017L38.1021 9.2789C35.3329 5.94191 31.8867 2.54166 26.4552 2.231C20.976 1.9176 13.1624 4.7313 1.65698 14.7574L0.343022 13.2496Z"
+        fill="black"
+        fill-opacity="0.5"
+      />
+    </svg>
   );
 }
