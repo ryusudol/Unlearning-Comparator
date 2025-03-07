9a240d45b9247ccdbc4844232b01a84be02566c2
Suhyeon <fbtngus2789@naver.com>
2025-03-07 13:29:21 +0900

    design: adjusted gaps between views in Forgetting and Retention

--- frontend/src/views/LayerWiseSimilarity.tsx
+++ frontend/src/views/LayerWiseSimilarity.tsx
@@ -19,7 +19,7 @@ export default function LayerWiseSimilarity() {
   const forgetClassExist = forgetClass !== -1;
 
   return (
-    <div className="h-[306.5px] relative top-4">
+    <div className="h-[322.5px] relative top-1">
       <div className="flex justify-between">
         <Subtitle title="Layer-Wise Similarity" className="left-0.5" />
         {forgetClassExist && areAllModelsSelected && (
