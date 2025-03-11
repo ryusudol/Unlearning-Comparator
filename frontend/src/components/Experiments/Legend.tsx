576d8d293e61343ca6b2cb7ca420c008149fb195
Suhyeon <fbtngus2789@naver.com>
2025-03-11 17:43:56 +0900

    design: changed the color scheme on the legend

--- frontend/src/components/Experiments/Legend.tsx
+++ frontend/src/components/Experiments/Legend.tsx
@@ -1,16 +1,26 @@
 import * as d3 from "d3";
 
 export default function DualMetricsLegend() {
-  const colorScale = d3
+  const greenScale = d3
+    .scaleSequential((t) => d3.interpolateGreens(0.03 + 0.77 * t))
+    .domain([0, 1]);
+  const accuracyGradient = `linear-gradient(to right, ${greenScale(
+    0
+  )} 0%, ${greenScale(1)} 100%)`;
+
+  const blueScale = d3
+    .scaleSequential((t) => d3.interpolateBlues(0.03 + 0.77 * t))
+    .domain([0, 1]);
+  const efficiencyGradient = `linear-gradient(to right, ${blueScale(
+    0
+  )} 0%, ${blueScale(1)} 100%)`;
+
+  const orangeScale = d3
     .scaleSequential((t) => d3.interpolateOranges(0.03 + 0.77 * t))
     .domain([0, 1]);
-  const steps = 8;
-  const orangeColors = Array.from({ length: steps }, (_, i) =>
-    colorScale(i / steps)
-  );
-  const forgettingQualityGradient = `linear-gradient(to right, ${orangeColors.join(
-    ", "
-  )})`;
+  const forgettingQualityGradient = `linear-gradient(to right, ${orangeScale(
+    0
+  )} 0%, ${orangeScale(1)} 100%)`;
 
   return (
     <div className="w-[530px] flex items-center gap-2 relative bottom-0.5 right-0">
@@ -19,15 +29,12 @@ export default function DualMetricsLegend() {
         <div className="relative h-[15px]">
           <div
             className="w-full h-full relative"
-            style={{
-              background:
-                "linear-gradient(to right, #f7fcf5, #e5f5e0, #c7e9c0, #a1d99b, #74c476, #41ab5d, #238b45, #006d2c)",
-            }}
+            style={{ background: accuracyGradient }}
           >
-            <span className="absolute left-1 text-[10px] text-gray-700 top-1/2 -translate-y-1/2">
+            <span className="absolute left-1 text-[10px] text-gray-700 top-2 -translate-y-1/2">
               Worst
             </span>
-            <span className="absolute right-1 text-[10px] text-white top-1/2 -translate-y-1/2">
+            <span className="absolute right-1 text-[10px] text-white top-2 -translate-y-1/2">
               Best
             </span>
           </div>
@@ -38,15 +45,12 @@ export default function DualMetricsLegend() {
         <div className="relative h-[15px]">
           <div
             className="w-full h-full relative"
-            style={{
-              background:
-                "linear-gradient(to right, #f7fbff, #deebf7, #c6dbef, #9ecae1, #6baed6, #4292c6, #2171b5, #08519c)",
-            }}
+            style={{ background: efficiencyGradient }}
           >
-            <span className="absolute left-1 text-[10px] text-gray-700 top-1/2 -translate-y-1/2">
+            <span className="absolute left-1 text-[10px] text-gray-700 top-2 -translate-y-1/2">
               Worst
             </span>
-            <span className="absolute right-1 text-[10px] text-white top-1/2 -translate-y-1/2">
+            <span className="absolute right-1 text-[10px] text-white top-2 -translate-y-1/2">
               Best
             </span>
           </div>
@@ -61,10 +65,10 @@ export default function DualMetricsLegend() {
             className="w-full h-full relative"
             style={{ background: forgettingQualityGradient }}
           >
-            <span className="absolute left-1 text-[10px] text-gray-700 top-1/2 -translate-y-1/2">
+            <span className="absolute left-1 text-[10px] text-gray-700 top-2 -translate-y-1/2">
               Worst
             </span>
-            <span className="absolute right-1 text-[10px] text-white top-1/2 -translate-y-1/2">
+            <span className="absolute right-1 text-[10px] text-white top-2 -translate-y-1/2">
               Best
             </span>
           </div>
